91ddc19cac13b43ee952f924bf45823d5d87f6fb
nikolaj26 <nikolaj@codeart.mk>
2024-10-17 09:32:29 +0200

    renamed the service provered

--- src/PasskeyLogin.php
+++ src/PasskeyLogin.php
@@ -3,21 +3,11 @@
 namespace Codeartnj\PasskeyLogin;
 
 
-use Codeartnj\PasskeyLogin\Pages\PasskeyLogin;
-use Codeartnj\PasskeyLogin\Widgets\PasskeyManagerWidget;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class PasskeyLoginProvider extends PackageServiceProvider
+class PasskeyLogin extends PackageServiceProvider
 {
-    protected array $pages = [
-        PasskeyLogin::class,
-    ];
-
-    protected array $widgets = [
-        PasskeyManagerWidget::class,
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package
